062e98bc04e4b6b963e0b33517c15db13c41fb53
Magdy156 <magdyhamdy156@gmail.com>
2023-03-16 06:08:44 -0700

    put modified with betty style ||

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - A program prints using puts() function
  *Return : 0 (success)
  */
-int main (void){
+int main(void){
 	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
