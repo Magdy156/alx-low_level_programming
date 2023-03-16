d7ae1c325826be3577da6ee2356c133042d439e8
Magdy156 <magdyhamdy156@gmail.com>
2023-03-16 06:10:17 -0700

    put modified with betty style |||

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@
  *Return : 0 (success)
  */
 int main(void){
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
