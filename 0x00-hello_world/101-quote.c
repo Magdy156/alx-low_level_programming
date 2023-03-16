75a027750fc97dfa936e7e545a956e882cfbe1d9
Magdy156 <magdyhamdy156@gmail.com>
2023-03-16 08:06:24 -0700

    101-unistd

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * * Description: main - C program that prints using fput
+ * * Return: 1 if success.
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
