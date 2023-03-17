9b7295e5a6a036f4b62b20488c53b941fbfd3a39
Magdy156 <magdyhamdy156@gmail.com>
2023-03-17 10:15:17 -0700

    alphabet modified

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,10 +10,10 @@ char alpha_lower[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'
 	, 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 char alpha_upper[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'
 	, 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-
-for (int i = 0; i < 26; i++)
+int i, j;
+for (i = 0; i < 26; i++)
 	putchar(alpha_lower[i]);
-for (int j = 0; j < 26; j++)
+for (j = 0; j < 26; j++)
 	putchar(alpha_upper[j]);
 putchar('\n');
 return (0);
