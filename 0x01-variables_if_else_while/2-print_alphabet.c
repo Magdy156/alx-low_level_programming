9917228c71fa4987ff9b874b76e762b5037a77f2
Magdy156 <magdyhamdy156@gmail.com>
2023-03-17 10:28:27 -0700

    lower alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * * Description: main - program prints alphabet
+ * * Return: 0 if success.
+ */
+int main(void)
+{
+char alpha_lower[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'
+	, 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+int i;
+for (i = 0; i < 26; i++)
+	putchar(alpha_lower[i]);
+putchar('\n');
+return (0);
+}
+
