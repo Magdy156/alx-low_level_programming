39f37f8ad461cefeb5b796a1ed8c809f9214392b
Magdy156 <magdyhamdy156@gmail.com>
2023-04-13 19:45:48 -0700

    task 3 modified

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,10 +18,7 @@ int *array_range(int min, int max)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i <= max; i++)
-	{
-		ptr[i] = min;
-		min++;
-	}
+	for (i = 0; min <= max; i++)
+		ptr[i] = min++;
 	return (ptr);
 }
