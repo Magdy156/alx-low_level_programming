2fca90777d280efcc59003bdc2bdbaa28d6c0863
Magdy156 <magdyhamdy156@gmail.com>
2023-05-11 14:19:46 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 /**
  * create_file - creates a file
  * @filename: A pointer to the name of the file
@@ -8,15 +7,20 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int f, bytes = 0, len = strlen(text_content);
+	int f, bytes, len = 0;
 
 	if (!filename)
 		return (-1);
-	f = open(filename, O_WRONLY, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (f == -1)
+
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
+	}
+	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	bytes = write(f, text_content, len);
+	if (f == -1 || bytes == -1)
 		return (-1);
-	if (len)
-		bytes = write(f, text_content, len);
 	close(f);
-	return (bytes == len ? 1 : -1);
+	return (1);
 }
