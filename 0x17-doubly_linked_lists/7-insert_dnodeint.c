32adbc57be4a2b9e61ff356ac8ba40f62036b004
Magdy156 <magdyhamdy156@gmail.com>
2023-06-17 21:59:20 +0300

    task 7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+unsigned int dlistint_length(const dlistint_t *h);
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: head of the linked list
+ * @idx: index
+ * @n: value to be inserted
+ * Return: address or NULL
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *newNode, *ptr = *h;
+	unsigned int length = dlistint_length(*h), i = 0;
+
+	if (idx > length - 1)
+		return (NULL);
+
+	newNode = malloc(sizeof(dlistint_t));
+	if (!newNode)
+		return (NULL);
+	newNode->n = n;
+
+	while (i < idx)
+	{
+		ptr = ptr->next;
+		i++;
+	}
+	newNode->next = ptr->next;
+	newNode->prev = ptr;
+	ptr->next->prev = newNode;
+	ptr->next = newNode;
+	return (newNode);
+}
+
+/**
+ * dlistint_length -  returns the number of elements i
+ * @h: the head of dlinked list to be printed
+ * Return: the number of nodes
+*/
+unsigned int dlistint_length(const dlistint_t *h)
+{
+	unsigned int counter = 0;
+
+	if (!h)
+		return (0);
+	while (h)
+	{
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
