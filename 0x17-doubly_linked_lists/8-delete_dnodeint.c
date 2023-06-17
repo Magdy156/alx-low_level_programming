6f3243ef77924b397f5d82cefec2dba5ed7c82da
Magdy156 <magdyhamdy156@gmail.com>
2023-06-17 22:35:55 +0300

    betty modified

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 /**
- * delete_dnodeint_at_index - deletes the node at index index of a dlistint_t linked list
+ * delete_dnodeint_at_index - deletes the node at index
+ * index of a dlistint_t linked list
  * @head: head of the lindked list
  * @index: index
  * Return: 1 or -1
