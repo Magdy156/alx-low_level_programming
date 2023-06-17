e87b2abd97d74d750bc1628304435a4b62d66c2b
Magdy156 <magdyhamdy156@gmail.com>
2023-06-17 20:51:18 +0300

    task 5 improved

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+unsigned int dlistint_length(const dlistint_t *h);
 /**
  * get_dnodeint_at_index -  gets the nth node of a dlistint_t linked list
  * @head: head of the list
@@ -8,7 +9,10 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
+	unsigned int length = dlistint_length(head);
 
+	if (index > length - 1)
+		return (NULL);
 	if (!head)
 		return (NULL);
 	while (i < index)
@@ -17,4 +21,22 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		i++;
 	}
 	return (head);
+}
+/**
+ * dlistint_length -  returns the number of elements i
+ * @h: the head of dlinked list to be printed
+ * Return: the number of nodes
+*/
+unsigned int dlistint_length(const dlistint_t *h)
+{
+	unsigned int counter = 0;
+
+	if (!h)
+		return (0);
+	while (h)
+	{
+		h = h->next;
+		counter++;
+	}
+	return (counter);
 }
